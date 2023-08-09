19269436ff85358b90db1dde043ff3a3a21c1e34
Yuhyeon0516 <rladbgus0516@naver.com>
2023-08-09 19:01:28 +0900

    created Google signin function

--- src/screens/IntroScreen.tsx
+++ src/screens/IntroScreen.tsx
@@ -1,20 +1,60 @@
 import {View} from 'react-native';
-import React from 'react';
+import React, {useCallback} from 'react';
 import {Header} from '../components/Header/Header';
-import {CustomButton} from '../components/CustomButton';
-import {Typography} from '../components/Typography';
 import {useRootNavigation} from '../navigation/RootNavigation';
+import {
+  GoogleSignin,
+  GoogleSigninButton,
+} from '@react-native-google-signin/google-signin';
+import {useSafeAreaInsets} from 'react-native-safe-area-context';
+import auth from '@react-native-firebase/auth';
 
 export default function IntroScreen() {
   const navigation = useRootNavigation<'Intro'>();
+  const safeAreaInset = useSafeAreaInsets();
+
+  async function handleGoogleSignInPress() {
+    const isSignIn = await GoogleSignin.isSignedIn();
+
+    if (isSignIn) {
+      await GoogleSignin.signOut();
+    }
+
+    const result = await GoogleSignin.signIn();
+    const googleCredential = auth.GoogleAuthProvider.credential(result.idToken);
+    const authResult = await auth().signInWithCredential(googleCredential);
+
+    navigation.push('Signup', {
+      screen: 'InputEmail',
+      params: {
+        preInput: {
+          email: result.user.email,
+          name: result.user.name ?? 'Unknown',
+          profileImage: result.user.photo ?? '',
+        },
+        uid: authResult.user.uid,
+      },
+    });
+  }
+
+  const onPressGoogleSignin = useCallback(handleGoogleSignInPress, [
+    navigation,
+  ]);
 
   return (
     <View style={{flex: 1}}>
       <Header>
         <Header.Title title="Intro" />
       </Header>
-      <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
-        <CustomButton
+      <View
+        style={{
+          flex: 1,
+          alignItems: 'center',
+          justifyContent: 'flex-end',
+          paddingBottom: 32 + safeAreaInset.bottom,
+        }}>
+        <GoogleSigninButton onPress={onPressGoogleSignin} />
+        {/* <CustomButton
           onPress={() =>
             navigation.push('Signup', {
               screen: 'InputEmail',
@@ -33,7 +73,7 @@ export default function IntroScreen() {
 
         <CustomButton onPress={() => navigation.replace('MainTab')}>
           <Typography fontSize={16}>메인</Typography>
-        </CustomButton>
+        </CustomButton> */}
       </View>
     </View>
   );
