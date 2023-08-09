22c6a5fac1dce47bd6c90ba630428fee2bfb8038
Yuhyeon0516 <rladbgus0516@naver.com>
2023-08-09 17:12:37 +0900

    created screen movement scenario

--- src/navigation/SignupNavigation.tsx
+++ src/navigation/SignupNavigation.tsx
@@ -16,6 +16,7 @@ export type TypeSignupNavigation = {
       profileImage: string;
     };
   };
+
   InputName: {
     uid: string;
     preInput: {
--- src/screens/InputNameScreen.tsx
+++ src/screens/InputNameScreen.tsx
@@ -1,13 +1,30 @@
 import {View} from 'react-native';
 import React from 'react';
 import {Header} from '../components/Header/Header';
+import {CustomButton} from '../components/CustomButton';
+import {Typography} from '../components/Typography';
+import {useRootNavigation} from '../navigation/RootNavigation';
+import {useSignupNavigation} from '../navigation/SignupNavigation';
 
 export default function InputNameScreen() {
+  const navigation = useRootNavigation<'Signup'>();
+  const signupNavigation = useSignupNavigation<'InputName'>();
   return (
     <View style={{flex: 1}}>
       <Header>
-        <Header.Title title="InputName" />
+        <Header.Group>
+          <Header.Icon
+            iconName="arrow-back"
+            onPress={() => signupNavigation.goBack()}
+          />
+          <Header.Title title="InputName" />
+        </Header.Group>
       </Header>
+      <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+        <CustomButton onPress={() => navigation.replace('MainTab')}>
+          <Typography fontSize={16}>회원가입 완료</Typography>
+        </CustomButton>
+      </View>
     </View>
   );
 }
