109681055fda94abe232b389c0fa552b16d559b7
Yuhyeon0516 <rladbgus0516@naver.com>
2023-08-10 21:00:33 +0900

    created history screen

--- src/actions/user.ts
+++ src/actions/user.ts
@@ -1,6 +1,17 @@
+import {ThunkAction, ThunkDispatch} from 'redux-thunk';
+import {TypeRootReducer} from '../store/store';
+import {TypeDog} from '../types/TypeDog';
 import {TypeUser} from '../types/TypeUser';
+import database from '@react-native-firebase/database';
 
 export const SET_USER_INFO = 'SET_USER_INFO' as const;
+
+export const GET_USER_LIKED_HISTORY_REQUEST =
+  'GET_USER_LIKED_HISTORY_REQUEST' as const;
+export const GET_USER_LIKED_HISTORY_SUCCESS =
+  'GET_USER_LIKED_HISTORY_SUCCESS' as const;
+export const GET_USER_LIKED_HISTORY_FAILURE =
+  'GET_USER_LIKED_HISTORY_FAILURE' as const;
 
 export function setUser(user: TypeUser) {
   return {
@@ -9,4 +20,69 @@ export function setUser(user: TypeUser) {
   };
 }
 
-export type TypeUserAction = ReturnType<typeof setUser>;
+export function getUserLikedHistoryRequest() {
+  return {
+    type: GET_USER_LIKED_HISTORY_REQUEST,
+  };
+}
+
+export function getUserLikedHistorySuccess(history: TypeDog[]) {
+  return {
+    type: GET_USER_LIKED_HISTORY_SUCCESS,
+    history: history,
+  };
+}
+
+export function getUserLikedHistoryFailure() {
+  return {
+    type: GET_USER_LIKED_HISTORY_FAILURE,
+  };
+}
+
+export const getUserLikedHistory =
+  (): TypeUserThunkAction => async (dispatch, getState) => {
+    dispatch(getUserLikedHistoryRequest());
+    const user = getState().user.user;
+
+    if (!user) {
+      dispatch(getUserLikedHistoryFailure());
+      return;
+    }
+
+    const ref = `history/${user.uid}`;
+
+    const currentHistory = await database()
+      .ref(ref)
+      .once('value')
+      .then(snapshot => snapshot.val());
+
+    const dogList = Object.keys(currentHistory).map(key => {
+      const item = currentHistory[key];
+
+      return {
+        photoUrl: item.url,
+        regeditAt: item.regeditAt,
+      } as TypeDog;
+    });
+
+    dispatch(getUserLikedHistorySuccess(dogList));
+  };
+
+export type TypeUserThunkAction = ThunkAction<
+  void,
+  TypeRootReducer,
+  undefined,
+  TypeUserAction
+>;
+
+export type TypeUserDispatch = ThunkDispatch<
+  TypeRootReducer,
+  undefined,
+  TypeUserAction
+>;
+
+export type TypeUserAction =
+  | ReturnType<typeof setUser>
+  | ReturnType<typeof getUserLikedHistoryRequest>
+  | ReturnType<typeof getUserLikedHistorySuccess>
+  | ReturnType<typeof getUserLikedHistoryFailure>;
